a73d34a6ce23ad0ecd088a078d2ed45c68b1263e
ankushbanerjee18 <148514255+ankushbanerjee18@users.noreply.github.com>
2025-06-15 22:58:51 +0530

    Add files via upload

---
+++ Batsmen.py
@@ -0,0 +1,53 @@
+# Creating a Cricket Batsmen database along with visualisation
+def batsman():
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    print("This tool will help you create a table for batsmen along with calculating their averages and then preparing a bar chart")
+    print("\n")
+    n= int(input("Enter the number of batsmen:"))
+    bat = {}
+    print("\n")
+    for i in range(n):
+        name = input(f"Enter the name of the batsman {i+1}:")
+        matches = int(input(f"Enter the number of matches played by {name}:"))
+        print("\n")
+        scores = {}
+        total = 0
+        out = 0
+        for j in range(matches):
+            runs = int(input(f"Enter the runs scored by {name} in match {j+1}:"))
+            scores[f"Match {j+1}"] = runs
+            outs = int(input(f"Enter '1' if {name} got out in the match, else enter '0':"))
+            total = total+runs
+            out = out+outs
+            print("\n")
+        if out == 0:
+            average = ("Infinity")
+        else:
+            average = total/out
+        scores['Total Runs']=total
+        scores['Average'] = average
+        print("\n")
+        print(f"The average of the {name} is {average}")
+        bat[name]=scores
+
+    print("\n")
+    print(bat)
+
+    df = pd.DataFrame(bat)
+    print("\n")
+    print(df)
+    print("\n")
+    print("Now a bar graph shall be created for a batsman of your choice")
+    df_edit = df.drop('Total Runs')
+    number = int(input("Enter the column number of your prefferd batsman from the table"))
+    sns.barplot(data=df_edit, x=df_edit.index, y=df_edit.columns[number-1])
+    batsman_name = df_edit.columns[number-1]
+    plt.title(f"Runs scored by {batsman_name} ")
+
+    print("\n")
+    print("Your data is also exported as a CSV file")
+    df.to_csv("bat.csv")
+    batsman()
+    
